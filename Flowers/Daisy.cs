3aaa5c2d25bc864588f04859b3a433ace7b78ea5
victo <victo@VICTORIAPC>
2023-10-04 19:25:09 +0100

    Add project files.

---
+++ Flowers/Daisy.cs
@@ -0,0 +1,8 @@
+﻿public class Daisy : Flower
+{
+    public Daisy(string name, int petals, int leaves) : base(name, petals, leaves)
+    {
+
+    }
+
+}
---
+++ Flowers/SunFlower.cs
@@ -0,0 +1,12 @@
+﻿public class SunFlower : Flower
+{
+    public SunFlower(string name, int petals, int leaves) : base(name, petals, leaves)
+    {
+
+    }
+
+    public override void Grow()
+    {
+        Height += 2;
+    }
+}
